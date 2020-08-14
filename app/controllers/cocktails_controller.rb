3c4c3e4f564b1157029460f7e70356153356c1fb
Yohan Franquelin <yranquelin@gmail.com>
2020-08-14 17:22:03 +0200

    Change color button

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -53,6 +53,6 @@ before_action :set_cocktail, only: [:show, :destroy]
 
    # Only allow a trusted parameter "white list" through.
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
